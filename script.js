5812b95fcb70a5e58e3827586505f05aadf3ed97
Ebba Wistrand <ebba.wistrand@elev.ga.ntig.se>
2026-05-28 10:51:48 +0200

    forts.

---
+++ script.js
@@ -0,0 +1,24 @@
+function light() {
+    document.body.style.backgroundColor = "#96c4db"
+        document.body.style.color = "#000000ff"
+            document.querySelector("h1").style.color = "#000000ff"
+                    document.querySelector("header").style.backgroundColor = "#ffffffff"
+    document.querySelector("main").style.backgroundColor = "#ffffffff"
+    document.querySelector("aside").style.backgroundColor = "#ffffffff"
+    document.querySelector("footer").style.backgroundColor = "#CAE0EB"
+        document.querySelector("figure").style.backgroundColor = "#CAE0EB"
+}
+function dark() {
+    document.body.style.backgroundColor = "#3a657a"
+    document.body.style.color = "#fff8b9"
+    document.querySelector("h1").style.color = "#fff8b9"
+
+        document.querySelector("header").style.backgroundColor = "#203f5e"
+    document.querySelector("main").style.backgroundColor = "#203f5e"
+    document.querySelector("aside").style.backgroundColor = "#203f5e"
+    document.querySelector("footer").style.backgroundColor = "#000000ff"
+        document.querySelector("figure").style.backgroundColor = "#000000ff"
+}
+function sign() {
+    
+}
